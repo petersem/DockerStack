2fec7c274a431ec310a9b4e3d5fcf349d1bdeaf9
Matt P <mpetersen@pobox.com>
2023-02-28 19:52:55 +1000

    Update revert_ports.sh
    Changed restart command

--- revert_ports.sh
+++ revert_ports.sh
@@ -6,5 +6,7 @@ HTTPS_PORT=443
 sed -i "s/^\([ \t]\+listen[ \t]\+[]:[]*\)81\([^0-9]\)/\1$HTTP_PORT\2/" /usr/syno/share/nginx/*.mustache
 sed -i "s/^\([ \t]\+listen[ \t]\+[]:[]*\)444\([^0-9]\)/\1$HTTPS_PORT\2/" /usr/syno/share/nginx/*.mustache
 
-sudo synoservicecfg --restart nginx
+# sudo synoservicecfg --restart nginx
 
+
+synosystemctl restart nginx
